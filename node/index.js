343e3053720e2997b0d9b91fa00bab170dc4ef1c
IanN20 <iann20@lakesideschool.org>
2020-04-12 17:49:15 -0700

    testing changes to javascript socket code

--- node/index.js
+++ node/index.js
@@ -1,8 +1,6 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('/home/student1920/.nvm/versions/node/v13.0.1/lib/node_modules/socket.io')(http);
-var cleintTimestamp = 0;
-var piTimestamp = 0;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + "/");
@@ -15,14 +13,11 @@ io.on('connection', function(socket){
     console.log('user disconnected');
   });
   socket.on('client message', function(msg){
-    cleintTimestamp = msg.substring(0,msg.indexOf("|"));
     console.log('client: '+msg);
     io.emit('client message', msg);
   });
   socket.on('pi message', function(msg){
     console.log("pi: "+msg);
-    piTimestamp = msg.substring(0,msg.indexOf("|"));
-    io.emit('server message', "Delay of "+(piTimestamp-cleintTimestamp) + " milliseconds");
     io.emit('pi message', msg);
   });
 });
